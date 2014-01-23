545a973a269ccc354eb1df9d08e17b6d87f0a4a6
Elizabeth Chaddock <echaddoc@ucsd.edu>
2014-01-23 11:18:56 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,7 +3,19 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+  $("#submitBtn").click(updateProject);
 })
+
+function updateProject(e) {
+   console.log('hi');
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+   console.log('hi again');
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
 
 /*
  * Function that is called when the document is ready.
@@ -11,8 +23,26 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").text("Please wait...");
+    $(".jumbotron p").toggleClass("active");
+  });
+    $("a.thumbnail").click(projectClick);
+	}
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
-}
+  function projectClick(e) {
+    //prevent the page from reloading
+    e.preventDefault();
+
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event
+  
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+      containingProject.append("<div class='project-description'><p>Description of the project.</p></div>"); 
+      $(".project-description").show();
+    } else { 
+       $(".project-description").hide();
+       
+    }
+  }
